a523e50c1f29b0d48895b84e521f69b8684db847
ialloyd <135848926+ialloyd@users.noreply.github.com>
2023-07-25 18:31:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 //your code here
+window.onload = function() {
+   let divs = document.querySelectorAll('.image');
+  for (let i = 0; i < divs.length; i++) {
+    divs[i].setAttribute('id', 'div' + (i + 1));
+    divs[i].addEventListener("dragstart", handleDragStart, false);
+    divs[i].addEventListener("dragover", handleDragOver, false);
+    divs[i].addEventListener("drop", handleDrop, false);
+  }
+}
+
+let dragSrcEl = null;
+
+function handleDragStart(e) {
+  this.style.opacity = '0.4';
+  dragSrcEl = this;
+  e.dataTransfer.effectAllowed = 'move';
+}
+
+function handleDragOver(e) {
+  if (e.preventDefault) {
+    e.preventDefault();
+  }
+  e.dataTransfer.dropEffect = 'move';
+  return false;
+}
+
+function handleDrop(e) {
+  if (e.stopPropagation) {
+    e.stopPropagation();
+  }
+  if (dragSrcEl != this) {
+    let srcBgImage = dragSrcEl.style.backgroundImage;
+    dragSrcEl.style.backgroundImage = this.style.backgroundImage;
+    this.style.backgroundImage = srcBgImage;
+  }
+  return false;
+}
